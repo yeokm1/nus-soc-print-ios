e8910b889e077082a1c0abf4c1a309984b5cc138
Yeo Kheng Meng <yeokm1@gmail.com>
2014-07-28 23:17:31 +0800

    Initial Commit

---
+++ NUS SOC Print/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  NUS SOC Print
+//
+//  Created by Yeo Kheng Meng on 28/7/14.
+//  Copyright (c) 2014 Yeo Kheng Meng. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
