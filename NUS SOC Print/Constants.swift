bf403ee6205b2358c531fe3f5975a8a418339728
Yeo Kheng Meng <yeokm1@gmail.com>
2014-08-13 13:25:19 +0800

    quota start

---
+++ NUS SOC Print/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  NUS SOC Print
+//
+//  Created by Yeo Kheng Meng on 13/8/14.
+//  Copyright (c) 2014 Yeo Kheng Meng. All rights reserved.
+//
+
+import Foundation
+
+
+let CREDENTIALS_WRONG = "Username/Password incorrect"
+let CREDENTIALS_MISSING = "Username/Password not set"
+let SERVER_UNREACHABLE = "Server unreachable. Check your internet connection"
--- NUS SOC Print/QuotaViewController.swift
+++ NUS SOC Print/QuotaViewController.swift
@@ -11,9 +11,32 @@ import UIKit
 
 class QuotaViewController: UIViewController {
     
+    @IBOutlet weak var quotaOutput: UITextView!
+    
+    
+    @IBAction func refreshButtonPress(sender: UIButton) {
+        refreshQuota()
+    }
+    
+    
+    func refreshQuota(){
+        var preferences : Storage = Storage.sharedInstance;
+        
+        
+        var username : String?  = preferences.getUsername()
+        var password : String? = preferences.getPassword()
+        
+        if(username == nil || username!.isEmpty || password == nil || password!.isEmpty){
+            quotaOutput.text = CREDENTIALS_MISSING
+        }
+
+
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        refreshQuota()
     }
     
     override func didReceiveMemoryWarning() {
