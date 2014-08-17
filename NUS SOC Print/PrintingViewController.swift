cd18ea063720c72fd3ec6d7335a76679c14e4364
Yeo Kheng Meng <yeokm1@gmail.com>
2014-08-17 21:36:57 +0800

    hide the indicators

--- NUS SOC Print/PrintingViewController.swift
+++ NUS SOC Print/PrintingViewController.swift
@@ -13,9 +13,35 @@ class PrintingViewController : UIViewController, UITableViewDataSource {
     
     let CELL_IDENTIFIER = "PrintingViewTableCell"
     
+    let HEADER_TEXT : Array<String> =
+    ["Connecting to server"
+    ,"Some housekeeping"
+    ,"Uploading doc converter"
+    ,"Uploading PDF Formatter"
+    ,"Formatting PDF"
+    ,"Converting to Postscript"
+    ,"Sending to printer"]
+    
+    
+    let PROGRESS_INDETERMINATE : Array<Bool> =
+    [true
+    ,true
+    ,false
+    ,false
+    ,true
+    ,true
+    ,true]
+    
+    
+    
+    
+    let TEXT_INDETERMINATE = "This could take awhile..."
+    
     var printer : String!
     var pagesPerSheet : String!
     var filePath : NSURL!
+    
+    var currentProgress : Int = 0
     
     @IBOutlet weak var progressTable: UITableView!
     
@@ -45,7 +71,29 @@ class PrintingViewController : UIViewController, UITableViewDataSource {
             cell = PrintingViewTableCell(style: UITableViewCellStyle.Default, reuseIdentifier: CELL_IDENTIFIER)
         }
         
+
+        processCell(cell!, row: indexPath.row)
+        
         return cell;
+    }
+    
+    
+    func processCell(cell : PrintingViewTableCell, row : Int){
+        cell.header.text = HEADER_TEXT[row]
+        cell.smallFooter.text = ""
+        
+        cell.progressBar.hidden = PROGRESS_INDETERMINATE[row]
+        cell.activityIndicator.hidden = true
+
+        //Adjust Tick
+        if(row >= currentProgress){
+            cell.tick.hidden = true
+        } else {
+            cell.tick.hidden = false
+        }
+        
+        
+        
     }
     
     
