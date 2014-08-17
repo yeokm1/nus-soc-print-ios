ae14092b1e40f98c69413c15eabd36fe37688562
Yeo Kheng Meng <yeokm1@gmail.com>
2014-08-17 15:45:21 +0800

    use NSOperation

--- NUS SOC Print/StatusViewController.swift
+++ NUS SOC Print/StatusViewController.swift
@@ -12,16 +12,14 @@ import UIKit
 class StatusViewController : UIViewController {
     
     let TEXT_GETTING_STATUS = "Retrieving Status"
-    let FORMAT_PRINTER_COMMAND = "lpq -P %@"
-    let FORMAT_PRINTER_OUTPUT = "%@ : %@\n"
-    let FORMAT_PRINTER_NO_OUTPUT = "%@ : No Output\n"
+    var operation : GetStatusOperation?
     
-    var connection : SSHConnectivity?
     
     @IBOutlet weak var statusOutputView: UITextView!
     
     @IBAction func closeButtonPressed(sender: UIBarButtonItem) {
-        connection?.disconnect()
+        operation?.cancel()
+
         self.dismissViewControllerAnimated(true, completion: nil)
     }
     @IBAction func refreshButtonPressed(sender: UIButton) {
@@ -31,7 +29,7 @@ class StatusViewController : UIViewController {
     
     func getStatus(){
         
-        if(connection != nil){
+        if(operation != nil){
             return
         }
         
@@ -41,79 +39,122 @@ class StatusViewController : UIViewController {
         var username : String?  = preferences.getUsername()
         var password : String? = preferences.getPassword()
         var hostname : String = preferences.getServer()
+        var printerList : Array<String> = preferences.getPrinterList()
         
         if(username == nil || username!.isEmpty || password == nil || password!.isEmpty){
             statusOutputView.text = FULL_CREDENTIALS_NOT_SET
         } else {
             statusOutputView.text = TEXT_GETTING_STATUS
             
+            operation = GetStatusOperation(hostname: hostname, username: username!, password: password!, printersList: printerList, outputView:statusOutputView)
+        
+            operation!.completionBlock = {(void) in
+                self.operation = nil
+            }
+
             
             dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), {(void) in
-
-                
-                self.connection = SSHConnectivity(hostname: hostname, username: username!, password: password!)
-                var connectionStatus = self.connection!.connect()
-                
-                var serverFound : Bool = connectionStatus.serverFound
-                var authorised : Bool = connectionStatus.authorised
-                
-                if(serverFound){
-                    if(!authorised){
-                        self.showOnOutputViewOnUIThread(CREDENTIALS_WRONG)
-                        return
-                    }
-                } else {
-                    self.showOnOutputViewOnUIThread(SERVER_UNREACHABLE)
-                    return
-                }
-                
-                
-                var printersArray : Array<String> = preferences.getPrinterList()
-                var outputString : String = ""
-                
-                for printer in printersArray {
-                    
-                    var command = String(format: self.FORMAT_PRINTER_COMMAND, printer)
-                    var commandOutput : String? = self.connection!.runCommand(command)
-                    
-                    var lineToShowToUI : String?
-                    
-                    if(commandOutput == nil){
-                        lineToShowToUI = String(format: self.FORMAT_PRINTER_NO_OUTPUT, printer)
-                    } else {
-                        lineToShowToUI = String(format: self.FORMAT_PRINTER_OUTPUT, printer, commandOutput!)
-                    }
-                    
-                    outputString += lineToShowToUI!
-                    self.showOnOutputViewOnUIThread(outputString)
-                    
-                }
-                
-                
-
-                
-                self.connection?.disconnect()
-                self.connection = nil
-                
-                
-                
-            });
+                self.operation!.start()
+            })
+    
             
         }
         
         
     }
     
-    
-    func showOnOutputViewOnUIThread(output: String){
-        dispatch_async(dispatch_get_main_queue(), {(void) in
-            self.statusOutputView.text = output
-        });
-    }
+
 
     
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    
+    class GetStatusOperation : NSOperation {
+    
+        let FORMAT_PRINTER_COMMAND = "lpq -P %@"
+        let FORMAT_PRINTER_OUTPUT = "%@ : %@\n"
+        let FORMAT_PRINTER_NO_OUTPUT = "%@ : No Output\n"
+        
+        var connection : SSHConnectivity?
+        var username : String?
+        var password : String?
+        var hostname : String?
+        var printersArray : Array<String>
+        weak var outputView : UITextView?
+        
+        
+        init(hostname : String, username : String, password : String, printersList : Array<String>, outputView : UITextView) {
+            self.username = username
+            self.password = password
+            self.hostname = hostname
+            self.outputView = outputView
+            self.printersArray = printersList
+
+        }
+        
+        override func main() {
+            connection = SSHConnectivity(hostname: hostname!, username: username!, password: password!)
+            var connectionStatus = connection!.connect()
+            
+            var serverFound : Bool = connectionStatus.serverFound
+            var authorised : Bool = connectionStatus.authorised
+            
+            if(serverFound){
+                if(!authorised){
+                    showOnOutputViewOnUIThread(CREDENTIALS_WRONG)
+                    return
+                }
+            } else {
+                showOnOutputViewOnUIThread(SERVER_UNREACHABLE)
+                return
+            }
+            
+            var outputString : String = ""
+            
+            for printer in printersArray {
+                
+                if(cancelled) {
+                    break
+                }
+                
+                var command = String(format: FORMAT_PRINTER_COMMAND, printer)
+                var commandOutput : String? = connection!.runCommand(command)
+                
+                var lineToShowToUI : String?
+                
+                if(commandOutput == nil){
+                    lineToShowToUI = String(format: FORMAT_PRINTER_NO_OUTPUT, printer)
+                } else {
+                    lineToShowToUI = String(format: FORMAT_PRINTER_OUTPUT, printer, commandOutput!)
+                }
+                
+                outputString += lineToShowToUI!
+                showOnOutputViewOnUIThread(outputString)
+                
+            }
+            
+            
+            
+            
+            self.connection?.disconnect()
+            self.connection = nil
+            
+            
+        }
+        
+
+        
+        
+        func showOnOutputViewOnUIThread(output: String){
+            dispatch_async(dispatch_get_main_queue(), {(void) in
+                self.outputView!.text = output
+            })
+        }
+        
+        
+        
     }
     
 }
