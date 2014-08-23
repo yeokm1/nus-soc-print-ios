61680a226c21c360642193d242d134b22a009225
Yeo Kheng Meng <yeokm1@gmail.com>
2014-08-23 17:16:05 +0800

    deletion command

--- NUS SOC Print/StatusViewController.swift
+++ NUS SOC Print/StatusViewController.swift
@@ -12,18 +12,24 @@ import UIKit
 class StatusViewController : UIViewController {
     
     let TEXT_GETTING_STATUS = "Retrieving Status"
-    var operation : GetStatusOperation?
+    var statusOperation : GetStatusOperation?
+    var deleteOperation : DeleteOperation?
     
+
     
     @IBOutlet weak var statusOutputView: UITextView!
     
     @IBAction func closeButtonPressed(sender: UIBarButtonItem) {
-        operation?.cancel()
-
+        statusOperation?.cancel()
+        deleteOperation?.cancel()
         dismissViewControllerAnimated(true, completion: nil)
     }
     @IBAction func refreshButtonPressed(sender: UIButton) {
         getStatus()
+    }
+    
+    @IBAction func deleteJobsPressed(sender: UIButton) {
+        startDeleting()
     }
     
     override func viewDidLoad() {
@@ -31,10 +37,9 @@ class StatusViewController : UIViewController {
         getStatus()
     }
     
-    
-    func getStatus(){
+    func startDeleting(){
         
-        if(operation != nil){
+        if(deleteOperation != nil){
             return
         }
         
@@ -51,15 +56,51 @@ class StatusViewController : UIViewController {
         } else {
             statusOutputView.text = TEXT_GETTING_STATUS
             
-            operation = GetStatusOperation(hostname: hostname, username: username!, password: password!, printersList: printerList, outputView:statusOutputView)
+            deleteOperation = DeleteOperation(hostname: hostname, username: username!, password: password!, printersList: printerList, outputView:statusOutputView)
+            
+            deleteOperation!.completionBlock = {(void) in
+                self.deleteOperation = nil
+            }
+            
+            
+            dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), {(void) in
+                self.deleteOperation!.start()
+            })
+            
+            
+        }
         
-            operation!.completionBlock = {(void) in
-                self.operation = nil
+        
+    }
+    
+    func getStatus(){
+        
+        if(statusOperation != nil){
+            return
+        }
+        
+        var preferences : Storage = Storage.sharedInstance;
+        
+        
+        var username : String?  = preferences.getUsername()
+        var password : String? = preferences.getPassword()
+        var hostname : String = preferences.getServer()
+        var printerList : Array<String> = preferences.getPrinterList()
+        
+        if(username == nil || username!.isEmpty || password == nil || password!.isEmpty){
+            statusOutputView.text = FULL_CREDENTIALS_NOT_SET
+        } else {
+            statusOutputView.text = TEXT_GETTING_STATUS
+            
+            statusOperation = GetStatusOperation(hostname: hostname, username: username!, password: password!, printersList: printerList, outputView:statusOutputView)
+        
+            statusOperation!.completionBlock = {(void) in
+                self.statusOperation = nil
             }
 
             
             dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), {(void) in
-                self.operation!.start()
+                self.statusOperation!.start()
             })
     
             
@@ -68,15 +109,7 @@ class StatusViewController : UIViewController {
         
     }
     
-    
-    
-    class GetStatusOperation : NSOperation {
-    
-        let FORMAT_PRINTER_COMMAND = "lpq -P %@"
-        let FORMAT_PRINTER_OUTPUT = "%@ : %@\n"
-        let FORMAT_PRINTER_NO_OUTPUT = "%@ : No Output\n"
-        let TEXT_NO_JOB = "Print Queue Empty\n"
-        
+    class BaseOperation : NSOperation {
         var connection : SSHConnectivity!
         var username : String!
         var password : String!
@@ -91,8 +124,80 @@ class StatusViewController : UIViewController {
             self.hostname = hostname
             self.outputView = outputView
             self.printersArray = printersList
-
+            
         }
+        
+        
+        func showOnOutputViewOnUIThread(output: String){
+            dispatch_async(dispatch_get_main_queue(), {(void) in
+                self.outputView!.text = output
+            })
+        }
+        
+    }
+    
+    class DeleteOperation : BaseOperation {
+        let FORMAT_PRINTER_COMMAND = "lprm -P %@ -"
+        let FORMAT_DELETION_OUTPUT = "Deletion command sent to %@\n"
+        let DELETION_COMMAND_SENT_TO_ALL = "Delection command set to all printers"
+        
+        override func main() {
+            connection = SSHConnectivity(hostname: hostname!, username: username!, password: password!)
+            var connectionStatus = connection.connect()
+            
+            var serverFound : Bool = connectionStatus.serverFound
+            var authorised : Bool = connectionStatus.authorised
+            
+            if(serverFound){
+                if(!authorised){
+                    showOnOutputViewOnUIThread(CREDENTIALS_WRONG)
+                    return
+                }
+            } else {
+                showOnOutputViewOnUIThread(SERVER_UNREACHABLE)
+                return
+            }
+            
+            var outputString : String = ""
+            
+            for printer in printersArray {
+                
+                if(cancelled) {
+                    break
+                }
+                
+                var command = String(format: FORMAT_PRINTER_COMMAND, printer)
+                connection!.runCommand(command)
+                
+                var lineToShowToUI : String = String(format: FORMAT_DELETION_OUTPUT, printer)
+                
+                outputString += lineToShowToUI
+                showOnOutputViewOnUIThread(outputString)
+                
+            }
+            
+            outputString += DELETION_COMMAND_SENT_TO_ALL
+            showOnOutputViewOnUIThread(outputString)
+            
+            
+            self.connection.disconnect()
+            self.connection = nil
+            
+            
+        }
+        
+        
+    }
+    
+    
+    
+    class GetStatusOperation : BaseOperation {
+    
+        let FORMAT_PRINTER_COMMAND = "lpq -P %@"
+        let FORMAT_PRINTER_OUTPUT = "%@ : %@\n"
+        let FORMAT_PRINTER_NO_OUTPUT = "%@ : No Output\n"
+        let TEXT_NO_JOB = "Print Queue Empty\n"
+        
         
         override func main() {
             connection = SSHConnectivity(hostname: hostname!, username: username!, password: password!)
@@ -150,13 +255,7 @@ class StatusViewController : UIViewController {
         }
         
 
-        
-        
-        func showOnOutputViewOnUIThread(output: String){
-            dispatch_async(dispatch_get_main_queue(), {(void) in
-                self.outputView!.text = output
-            })
-        }
+    
         
         
         
